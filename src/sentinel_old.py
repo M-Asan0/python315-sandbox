8ee208a900333d403d02fb3d7043e5ec9bc0c196
M-Asan0 <asanomasaaki6@gmail.com>
2026-07-05 14:59:49 +0900

    update modules

--- src/sentinel_old.py
+++ src/sentinel_old.py
@@ -2,17 +2,16 @@ import copy
 
 _MISSING = object()
 
-def greet(name: object = _MISSING):     # 型注釈としてはobjectとするしかなく、ただしobjectはあらゆる型を含んでしまう
+def greet(name: object = _MISSING):     # 専用の型を表現できないため、objectなどで受けることになる
     if name is _MISSING:
         print("何も渡されてない")
     else:
         print(f"渡された: {name}")
 
-greet()             # → 何も渡されてない
-greet(None)         # → 渡された: None
+greet()             # 何も渡されてない
+greet(None)         # 渡された: None
 
-# 困る場面 printしたとき
-print(_MISSING)     # print(_MISSING)
+print(_MISSING)     # <object object at 0x722c62e90250>
 
 # オブジェクトのコピー
 config = {"timeout": _MISSING}
